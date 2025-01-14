38412ff3b37b5335dc6ebf65f26f03498e041088
Ivan Prokudin <ivan@prokudin.org>
2025-01-14 09:45:42 +0200

    restricted to admin decorator

--- decorators.py
+++ decorators.py
@@ -9,3 +9,20 @@ def restrict_to_channel(func):
             return
         return await func(update, context)
     return wrapper
+
+# Декоратор для ограничения команд только администраторам канала
+def restrict_to_admins(func):
+    async def wrapper(update: Update, context):
+        chat_id = CHANNEL_ID
+        user_id = update.effective_user.id
+
+        # Получаем список администраторов канала
+        chat_administrators = await context.bot.get_chat_administrators(chat_id)
+        admin_ids = [admin.user.id for admin in chat_administrators]
+
+        if user_id not in admin_ids:
+            await update.message.reply_text("Эта команда доступна только администраторам канала.")
+            return
+
+        return await func(update, context)
+    return wrapper
