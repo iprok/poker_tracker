0aff6d49cf1bc840d301cf32e405f16e7820e1e8
Ivan Prokudin <ivan@prokudin.org>
2025-01-14 02:47:07 +0200

    fixed timezone in logs

--- utils.py
+++ utils.py
@@ -7,4 +7,6 @@ def format_datetime(dt: datetime) -> str:
     Форматирует дату и время в указанный временной пояс и возвращает строку формата DD-MM-YYYY HH:MM:SS.
     """
     timezone = pytz.timezone(TIMEZONE)
+    if dt.tzinfo is None:
+        dt = pytz.utc.localize(dt)  # Применяем UTC, если временная зона отсутствует
     return dt.astimezone(timezone).strftime("%d-%m-%Y %H:%M:%S")
