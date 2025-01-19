6140aee3553cdcdf586e73d8445a7b20b305a5ea
Ivan Prokudin <ivan@prokudin.org>
2025-01-19 16:40:25 +0200

    tryed to create find_actions_by_game

--- domain/repository/player_action_repository.py
+++ domain/repository/player_action_repository.py
@@ -1,6 +1,14 @@
-from domain.entity.game import Game
-from domain.repository.base_repository import BaseRepository
+from typing import List
 
+from domain.entity.PlayerAction import PlayerAction
+from domain.repository.base_repository import BaseRepository
+from sqlalchemy import or_
 
 class PlayerActionRepository(BaseRepository):
-    pass
+    def find_actions_by_game(self, game_id) -> List[PlayerAction]:
+        return self.session.query(PlayerAction)
+            .filter(
+                PlayerAction.game_id == game_id,
+                or_(PlayerAction.action == "buyin", PlayerAction.action == "quit")
+            )
+            .all()
