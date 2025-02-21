4edea4f6ca4591d51c4c40445197772af0db3722
Ivan Prokudin <ivan@prokudin.org>
2025-02-21 17:41:58 +0200

    decreased limit for log

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ USE_TABLE = config.get("use_table", True)
 SHOW_SUMMARY_ON_BUYIN = config["show_summary_on_buyin"]
 SHOW_SUMMARY_ON_QUIT = config["show_summary_on_quit"]
 LOG_AMOUNT_LAST_GAMES = config.get("log_amount_last_games", 3)
-LOG_AMOUNT_LAST_ACTIONS = config.get("log_amount_last_actions", 25)
+LOG_AMOUNT_LAST_ACTIONS = config.get("log_amount_last_actions", 20)
