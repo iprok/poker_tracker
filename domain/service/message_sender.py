19b5da3cffb193566e58c10ca778418887d377bf
Ivan Prokudin <ivan@prokudin.org>
2025-04-07 02:28:24 +0300

    Last stage of code cleaning for pyright

--- domain/service/message_sender.py
+++ domain/service/message_sender.py
@@ -7,18 +7,21 @@ class MessageSender:
 
     @staticmethod
     def send_to_channel(
-        update: Update, context: ContextTypes.DEFAULT_TYPE, text, parse_mode=None
+        _: Update, context: ContextTypes.DEFAULT_TYPE, text, parse_mode=None
     ):
         return context.bot.send_message(CHANNEL_ID, text, parse_mode=parse_mode)
 
     @staticmethod
     def send_to_current_channel(
         update: Update,
-        context: ContextTypes.DEFAULT_TYPE,
+        _: ContextTypes.DEFAULT_TYPE,
         text,
         reply_markup=None,
         parse_mode=None,
     ):
-        return update.message.reply_text(
-            text, reply_markup=reply_markup, parse_mode=parse_mode
-        )
+        if update.message:
+            return update.message.reply_text(
+                text, reply_markup=reply_markup, parse_mode=parse_mode
+            )
+        else:
+            return None
