ca2597265154990dc52ecbf2052f5f9667a12398
Ivan Prokudin <ivan@prokudin.org>
2025-04-07 01:41:34 +0300

    Some special timezone aware things

--- domain/entity/game.py
+++ domain/entity/game.py
@@ -1,17 +1,22 @@
 from sqlalchemy import Column, Integer, DateTime
-from datetime import datetime
+from datetime import datetime, timezone
 from engine import Base, Engine
 
 
 class Game(Base):
     __tablename__ = "games"
     id = Column(Integer, primary_key=True)
-    start_time = Column(DateTime, default=datetime.utcnow)
+    start_time = Column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+    )
+
     end_time = Column(DateTime, nullable=True)
 
     def get_duration(self):
         """Возвращает продолжительность игры в формате 'HH:MM:SS'"""
-        end_time = self.end_time if self.end_time else datetime.utcnow()
+        end_time = (
+            self.end_time if self.end_time is not None else datetime.now(timezone.utc)
+        )
         duration = end_time - self.start_time
         total_seconds = int(duration.total_seconds())
         hours = total_seconds // 3600
